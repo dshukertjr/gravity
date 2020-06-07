c4f6549927cdcb7dca3d79672146f6a6d4181589
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-06-07 21:34:08 +0900

    added gesture detector

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,16 @@ import 'package:flutter/material.dart';
 
 import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
+import 'package:gravity/box_game.dart';
+import 'package:flutter/gestures.dart';
 
-void main() async {
+void main() {
+  BoxGame game = BoxGame();
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTapDown;
+  runApp(game.widget);
+
   Util flameUtil = Util();
-  await flameUtil.fullScreen();
-  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
-
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: ScreenWidget(),
-    );
-  }
-}
-
-class ScreenWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
+  flameUtil.fullScreen();
+  flameUtil.setOrientation(DeviceOrientation.portraitUp);
 }
